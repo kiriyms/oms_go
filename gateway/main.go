ea74b3e26a27a6f6e8327d48e4d13b5449d41b70
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2026-01-09 18:06:07 +0200

    feat: add main to gateway

---
+++ gateway/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const (
+	httpAddr = ":8080"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	handler := NewHandler()
+	handler.registerRoutes(mux)
+
+	if err := http.ListenAndServe(httpAddr, mux); err != nil {
+		log.Fatal("Failed to start Gateway HTTP server")
+	}
+}
