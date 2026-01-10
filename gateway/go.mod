2bc4dfc4e2726441a34ca1062bc190e943c93f7a
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2026-01-10 15:15:55 +0200

    feat: add .env variable reading in gateway

--- gateway/go.mod
+++ gateway/go.mod
@@ -1,3 +1,5 @@
 module github.com/kiriyms/oms_go-gateway
 
 go 1.25.1
+
+require github.com/joho/godotenv v1.5.1
--- gateway/main.go
+++ gateway/main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+
+	_ "github.com/joho/godotenv/autoload"
+	common "github.com/kiriyms/oms_go-common"
 )
 
-const (
-	httpAddr = ":8080"
+var (
+	httpAddr = common.GetEnv("HTTP_ADDR", ":8080")
 )
 
 func main() {
 	mux := http.NewServeMux()
 	handler := NewHandler()
 	handler.registerRoutes(mux)
+
+	log.Printf("Starting Server at %s", httpAddr)
 
 	if err := http.ListenAndServe(httpAddr, mux); err != nil {
 		log.Fatal("Failed to start Gateway HTTP server")
