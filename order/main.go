a50d9023e94f99cb2388c1339a8a01c2f758a58c
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2026-01-12 15:27:55 +0200

    feat: add grpc server to Order service

--- order/main.go
+++ order/main.go
@@ -1,10 +1,32 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net"
+
+	common "github.com/kiriyms/oms_go-common"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = common.GetEnv("GRPC_ADDR", "localhost:50051")
+)
 
 func main() {
+	grpcServer := grpc.NewServer()
+	l, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	defer l.Close()
+
 	store := NewStore()
 	service := NewOrderService(store)
 
 	service.CreateOrder(context.Background())
+
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err.Error())
+	}
 }
