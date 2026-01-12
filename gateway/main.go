f93d031b38081a4d775f777b368cafaf070b94ec
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2026-01-12 15:27:33 +0200

    feat: add grpc client to gateway service

--- gateway/main.go
+++ gateway/main.go
@@ -6,15 +6,29 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	common "github.com/kiriyms/oms_go-common"
+	pb "github.com/kiriyms/oms_go-common/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	httpAddr = common.GetEnv("HTTP_ADDR", ":8080")
+	httpAddr         = common.GetEnv("HTTP_ADDR", ":8080")
+	orderServiceAddr = common.GetEnv("ORDER_SERVICE_ADDR", "localhost:50051")
 )
 
 func main() {
+	conn, err := grpc.NewClient(orderServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to Order Service: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Dialed Order Service at ", orderServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Starting Server at %s", httpAddr)
