731e15e92a1acc730bcfdb99e70555b4870ef268
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2026-01-14 14:36:52 +0200

    feat: add ValidateOrder to Order Service

--- order/service.go
+++ order/service.go
@@ -1,9 +1,16 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+
+	common "github.com/kiriyms/oms_go-common"
+	pb "github.com/kiriyms/oms_go-common/api"
+)
 
 type OrderService interface {
 	CreateOrder(context.Context) error
+	ValidateOrder(context.Context, *pb.CreateOrderRequest) error
 }
 
 type service struct {
@@ -16,4 +23,35 @@ func NewOrderService(store OrderStore) *service {
 
 func (s *service) CreateOrder(ctx context.Context) error {
 	return s.store.Create(ctx)
+}
+
+func (s *service) ValidateOrder(ctx context.Context, p *pb.CreateOrderRequest) error {
+	if len(p.Items) == 0 {
+		return common.ErrNoItems
+	}
+
+	merged := mergeItemsQuantities(p.Items)
+	log.Printf("Merged items: %v", merged)
+
+	// validate with store service
+
+	return nil
+}
+
+func mergeItemsQuantities(items []*pb.ItemWithQuantity) []*pb.ItemWithQuantity {
+	merged := make([]*pb.ItemWithQuantity, 0)
+	itemMap := make(map[string]int32)
+
+	for _, item := range items {
+		itemMap[item.ID] += item.Quantity
+	}
+
+	for id, quantity := range itemMap {
+		merged = append(merged, &pb.ItemWithQuantity{
+			ID:       id,
+			Quantity: quantity,
+		})
+	}
+
+	return merged
 }
