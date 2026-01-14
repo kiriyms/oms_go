da6b0179ba51c0a947241b08a20c71df97befe71
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2026-01-14 14:36:11 +0200

    feat: add Order service validation, add uuid generation

--- order/go.mod
+++ order/go.mod
@@ -1,3 +1,5 @@
 module github.com/kiriyms/oms_go-order
 
 go 1.25.1
+
+require github.com/google/uuid v1.6.0 // indirect
--- order/handler.go
+++ order/handler.go
@@ -4,26 +4,33 @@ import (
 	"context"
 	"log"
 
+	"github.com/google/uuid"
 	pb "github.com/kiriyms/oms_go-common/api"
 	"google.golang.org/grpc"
 )
 
 type Handler struct {
 	pb.UnimplementedOrderServiceServer
+	service OrderService
 }
 
-func NewHandler(s *grpc.Server) *Handler {
-	h := &Handler{}
+func NewHandler(s *grpc.Server, service OrderService) *Handler {
+	h := &Handler{
+		service: service,
+	}
 	pb.RegisterOrderServiceServer(s, h)
 	return h
 }
 
 func (h *Handler) CreateOrder(ctx context.Context, p *pb.CreateOrderRequest) (*pb.Order, error) {
 	log.Printf("New order received: %v", p)
+	if err := h.service.ValidateOrder(ctx, p); err != nil {
+		return nil, err
+	}
 	o := &pb.Order{
-		ID:         "123",
-		CustomerID: "jd456",
-		Status:     "completed",
+		ID:         uuid.New().String(),
+		CustomerID: p.CustomerID,
+		Status:     "PENDING",
 		Items:      []*pb.Item{},
 	}
 	return o, nil
