b5784387159264ec78b5b8fd35ca8a7e0a519bb7
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2026-01-14 14:38:27 +0200

    feat: add order request validation to gateway service

--- gateway/handler.go
+++ gateway/handler.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
 	common "github.com/kiriyms/oms_go-common"
 	pb "github.com/kiriyms/oms_go-common/api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type handler struct {
@@ -31,8 +34,42 @@ func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+	if err := validateItems(items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	o, err := h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
 		CustomerID: cID,
 		Items:      items,
 	})
+	rStatus := status.Convert(err)
+	if rStatus != nil {
+		if rStatus.Code() != codes.InvalidArgument {
+			common.WriteError(w, http.StatusBadRequest, rStatus.Message())
+			return
+		}
+
+		common.WriteError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	common.WriteJSON(w, http.StatusCreated, o)
+}
+
+func validateItems(items []*pb.ItemWithQuantity) error {
+	if len(items) == 0 {
+		return common.ErrNoItems
+	}
+
+	for _, item := range items {
+		if item.ID == "" {
+			return errors.New("item ID cannot be empty")
+		}
+		if item.Quantity <= 0 {
+			return errors.New("item quantity must be greater than zero")
+		}
+	}
+
+	return nil
 }
